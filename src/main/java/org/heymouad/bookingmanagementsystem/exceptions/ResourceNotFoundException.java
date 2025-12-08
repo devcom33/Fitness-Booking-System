d66db5d4cf3cde2fca4988df4810f5c51ea91e5f
devcom33 <devcom33@users.noreply.github.com>
2025-12-08 16:59:43 +0100

    chore: Add mapping to 404 http status

--- src/main/java/org/heymouad/bookingmanagementsystem/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,11 @@
 package org.heymouad.bookingmanagementsystem.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.UUID;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String resourceName, UUID id) {
         super(String.format("%s Not Found with id: %s", resourceName, id));
