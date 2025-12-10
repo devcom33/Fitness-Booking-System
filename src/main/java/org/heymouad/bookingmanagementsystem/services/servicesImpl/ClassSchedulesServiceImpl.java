5f31684f499b648d93935edb0e52ad50357fad16
devcom33 <devcom33@users.noreply.github.com>
2025-12-10 16:06:19 +0100

    refactor: update ClassSchedulesServiceImpl to use Entities instead of DTOs

--- src/main/java/org/heymouad/bookingmanagementsystem/services/servicesImpl/ClassSchedulesServiceImpl.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/services/servicesImpl/ClassSchedulesServiceImpl.java
@@ -3,14 +3,13 @@ package org.heymouad.bookingmanagementsystem.services.servicesImpl;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import net.fortuna.ical4j.model.Recur;
-import org.heymouad.bookingmanagementsystem.dtos.ClassSchedulesDto;
-import org.heymouad.bookingmanagementsystem.dtos.RecurringScheduleTemplateDto;
 import org.heymouad.bookingmanagementsystem.entities.ClassSchedules;
+import org.heymouad.bookingmanagementsystem.entities.FitnessClasses;
+import org.heymouad.bookingmanagementsystem.entities.Instructor;
 import org.heymouad.bookingmanagementsystem.entities.RecurringScheduleTemplate;
 import org.heymouad.bookingmanagementsystem.exceptions.InstructorBusyException;
 import org.heymouad.bookingmanagementsystem.exceptions.InvalidScheduleException;
-import org.heymouad.bookingmanagementsystem.mappers.ClassSchedulesMapper;
-import org.heymouad.bookingmanagementsystem.mappers.RecurringScheduleTemplateMapper;
+import org.heymouad.bookingmanagementsystem.exceptions.ResourceNotFoundException;
 import org.heymouad.bookingmanagementsystem.repositories.ClassSchedulesRepository;
 import org.heymouad.bookingmanagementsystem.repositories.FitnessClassesRepository;
 import org.heymouad.bookingmanagementsystem.repositories.InstructorRepository;
@@ -30,98 +29,111 @@ import java.util.UUID;
 @Service
 public class ClassSchedulesServiceImpl implements ClassSchedulesService {
     private final ClassSchedulesRepository classSchedulesRepository;
-    private final ClassSchedulesMapper classSchedulesMapper;
     private final InstructorRepository instructorRepository;
     private final FitnessClassesRepository fitnessClassesRepository;
     private final RecurringScheduleTemplateRepository recurringScheduleTemplateRepository;
-    private final RecurringScheduleTemplateMapper recurringScheduleTemplateMapper;
 
     /**
-     * This Method Handles the creation of a new Class Schedule, managing both single, one-off classes
+     * Handles the creation of a new Class Schedule, managing both single, one-off classes
      * and long running recurring class series.
-     * @param classSchedulesDto the DTO containing class details and optional recurrence rule.
-     * @return A list of created schedules (one for a single class, many for a series)
+     * @param classSchedules the entity
+     * @return A list of created entities
      */
     @Override
-    public List<ClassSchedulesDto> createClassSchedules(ClassSchedulesDto classSchedulesDto) {
-        ZonedDateTime startTime = classSchedulesDto.startTime();
-        ZonedDateTime endTime = classSchedulesDto.endTime();
+    public List<ClassSchedules> createClassSchedules(ClassSchedules classSchedules) {
+        ZonedDateTime startTime = classSchedules.getStartTime();
+        ZonedDateTime endTime = classSchedules.getEndTime();
 
         if (!startTime.isBefore(endTime))
         {
             throw new InvalidScheduleException("Start time must be before end time.");
         }
 
-        UUID instructorId = classSchedulesDto.instructorDto().id();
+        UUID instructorId = classSchedules.getInstructor().getId();
 
         if (!instructorRepository.existsById(instructorId)) {
             throw new EntityNotFoundException("Instructor with ID " + instructorId + " not found.");
         }
 
-        UUID classId = classSchedulesDto.fitnessClassesDto().id();
+        UUID classId = classSchedules.getFitnessClasses().getId();
         if (!fitnessClassesRepository.existsById(classId)) {
             throw new EntityNotFoundException("Fitness Class with ID " + classId + " not found.");
         }
 
-        if (classSchedulesDto.recurringTemplate().rrule().isBlank())
+        if (classSchedules.getTemplate().getRrule().isBlank())
         {
-            return saveOneOffSchedule(classSchedulesDto);
+            return saveOneOffSchedule(classSchedules);
         }
 
-        return saveRecurringSchedules(classSchedulesDto);
+        return saveRecurringSchedules(classSchedules);
     }
 
     /**
-     * Retrieves all existing class schedules from the database.
-     * @return A list of all ClassSchedulesDto objects.
+     * Retrieves a single Class Schedule by its unique identifier
+     * @param id The ID of the Class Schedule to retrieve
+     * @return The entity
+     * @throws ResourceNotFoundException If the schedule is not found
      */
     @Transactional(readOnly = true)
     @Override
-    public List<ClassSchedulesDto> getAllClassSchedules() {
-        return classSchedulesRepository
-                .findAll()
-                .stream()
-                .map(classSchedulesMapper::toClassSchedulesDto).toList();
+    public ClassSchedules getClassScheduleById(UUID id) {
+        return classSchedulesRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Class Schedule", id));
+    }
+
+
+    /**
+     * Retrieves all existing class schedules from the database
+     * @return A list of all entities
+     */
+    @Transactional(readOnly = true)
+    @Override
+    public List<ClassSchedules> getAllClassSchedules() {
+        return classSchedulesRepository.findAll();
     }
 
     /**
-     * Handles the persistence and validation for a single  class schedule.
-     * @param classSchedulesDto The DTO containing the single schedule details.
-     * @return A list containing the single created ClassSchedulesDto.
+     * Handles the persistence and validation for a single class schedule
+     * @param classSchedules The Entity containing the single schedule details
+     * @return A list containing the single created entity
+     * @throws InstructorBusyException If the instructor is already scheduled during this time slot
      */
-    private List<ClassSchedulesDto> saveOneOffSchedule(ClassSchedulesDto classSchedulesDto) {
-        ZonedDateTime startTime = classSchedulesDto.startTime();
-        ZonedDateTime endTime = classSchedulesDto.endTime();
-        UUID instructorId = classSchedulesDto.instructorDto().id();
+    private List<ClassSchedules> saveOneOffSchedule(ClassSchedules classSchedules) {
+        ZonedDateTime startTime = classSchedules.getStartTime();
+        ZonedDateTime endTime = classSchedules.getEndTime();
+        UUID instructorId = classSchedules.getInstructor().getId();
         boolean overlap = classSchedulesRepository.existsClassSchedulesByInstructorIdAndOverlap(startTime, endTime, instructorId);
 
         if (overlap)
         {
             throw new InstructorBusyException("Instructor is already scheduled during this time slot.");
-        }        ClassSchedules classSchedules = classSchedulesMapper.toClassSchedules(classSchedulesDto);
-        return List.of(classSchedulesMapper.toClassSchedulesDto(classSchedulesRepository.save(classSchedules)));
+        }
+
+        return List.of(classSchedulesRepository.save(classSchedules));
     }
 
     /**
      * Handles the generation, validation, and persistence for an entire recurring schedule series.
-     * @param classSchedulesDto The DTO containing the recurrence template and class details.
-     * @return A list of all created ClassSchedulesDto instances in the series.
-     * @throws InstructorBusyException If any generated instance conflicts with an existing schedule.
+     * @param classSchedules The initial ClassSchedules entity containing the recurrence template and class details
+     * @return A list of all created entities in the series
+     * @throws InstructorBusyException If any generated instance conflicts with an existing schedule
      */
-    private List<ClassSchedulesDto> saveRecurringSchedules(ClassSchedulesDto classSchedulesDto)
+    private List<ClassSchedules> saveRecurringSchedules(ClassSchedules classSchedules)
     {
-        RecurringScheduleTemplateDto template = classSchedulesDto.recurringTemplate();
-        RecurringScheduleTemplate savedTemplate = recurringScheduleTemplateRepository.save(recurringScheduleTemplateMapper.toEntity(template));
-        Recur recur = new Recur(template.rrule());
-        ZonedDateTime periodStart = template.startDateTime();
-        ZonedDateTime periodEnd = template.endDateTime();
-
+        RecurringScheduleTemplate template = classSchedules.getTemplate();
+        RecurringScheduleTemplate savedTemplate = recurringScheduleTemplateRepository.save(template);
+        Recur recur = new Recur(template.getRrule());
+        ZonedDateTime periodStart = template.getStartDateTime();
+        ZonedDateTime periodEnd = template.getEndDateTime();
 
         var dates = recur.getDates(periodStart, periodEnd);
 
-        Duration duration = Duration.between(classSchedulesDto.startTime(), classSchedulesDto.endTime());
+        Duration duration = Duration.between(classSchedules.getStartTime(), classSchedules.getEndTime());
 
-        List<ClassSchedules> classSchedulesDtoList = new ArrayList<>();
+        List<ClassSchedules> schedulesToSave = new ArrayList<>();
+
+        FitnessClasses fitnessClass = classSchedules.getFitnessClasses();
+        Instructor instructor = classSchedules.getInstructor();
 
         for (var date : dates)
         {
@@ -133,28 +145,24 @@ public class ClassSchedulesServiceImpl implements ClassSchedulesService {
             boolean overlap = classSchedulesRepository.existsClassSchedulesByInstructorIdAndOverlap(
                     instantStart,
                     instantEnd,
-                    classSchedulesDto.instructorDto().id()
+                    classSchedules.getInstructor().getId()
             );
 
             if (overlap)
             {
                 throw new InstructorBusyException("Instructor is busy on a recurring date");
             }
+            ClassSchedules classScheduleInstance = new ClassSchedules();
 
-            ClassSchedules classSchedule = classSchedulesMapper.toClassSchedules(classSchedulesDto);
+            classScheduleInstance.setFitnessClasses(fitnessClass);
+            classScheduleInstance.setInstructor(instructor);
+            classScheduleInstance.setTemplate(savedTemplate);
+            classScheduleInstance.setStartTime(instantStart);
+            classScheduleInstance.setEndTime(instantEnd);
 
-            classSchedule.setStartTime(instantStart);
-            classSchedule.setEndTime(instantEnd);
-            classSchedule.setTemplate(savedTemplate);
-
-            classSchedulesDtoList.add(classSchedule);
+            schedulesToSave.add(classScheduleInstance);
         }
 
-        List<ClassSchedules> classSchedules = classSchedulesRepository.saveAll(classSchedulesDtoList);
-
-        return classSchedules.stream()
-                .map(classSchedulesMapper::toClassSchedulesDto)
-                .toList();
+        return classSchedulesRepository.saveAll(schedulesToSave);
     }
-
 }
