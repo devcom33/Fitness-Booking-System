6fa95dbdd7e3d3e5b951b540ff79cf89f7af169d
devcom33 <devcom33@users.noreply.github.com>
2025-12-12 00:22:16 +0100

    chore: update ClassSchedulesDto to ClassSchedules in service interface

--- src/main/java/org/heymouad/bookingmanagementsystem/services/ClassSchedulesService.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/services/ClassSchedulesService.java
@@ -1,12 +1,15 @@
 package org.heymouad.bookingmanagementsystem.services;
 
 import org.heymouad.bookingmanagementsystem.dtos.ClassSchedulesDto;
+import org.heymouad.bookingmanagementsystem.entities.ClassSchedules;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public interface ClassSchedulesService {
-    List<ClassSchedulesDto> createClassSchedules(ClassSchedulesDto classSchedulesDto);
-    List<ClassSchedulesDto> getAllClassSchedules();
+    List<ClassSchedules> createClassSchedules(ClassSchedules classSchedules);
+    ClassSchedules getClassScheduleById(UUID id);
+    List<ClassSchedules> getAllClassSchedules();
 }
