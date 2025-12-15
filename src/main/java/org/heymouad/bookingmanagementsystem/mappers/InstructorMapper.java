47401fbdf606cf171fa16291bbee3423bae0d7a8
devcom33 <devcom33@users.noreply.github.com>
2025-12-15 23:31:10 +0100

    fix: fix InstructorMapper mapping between dto and entity

---
+++ src/main/java/org/heymouad/bookingmanagementsystem/mappers/InstructorMapper.java
@@ -0,0 +1,20 @@
+package org.heymouad.bookingmanagementsystem.mappers;
+
+import org.heymouad.bookingmanagementsystem.dtos.InstructorDto;
+import org.heymouad.bookingmanagementsystem.entities.Instructor;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+
+@Mapper(componentModel = "spring")
+public interface InstructorMapper {
+
+    @Mapping(target = "userID", source = "user.id")
+    InstructorDto toDto(Instructor instructor);
+
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    Instructor toEntity(InstructorDto instructorDto);
+}
+
