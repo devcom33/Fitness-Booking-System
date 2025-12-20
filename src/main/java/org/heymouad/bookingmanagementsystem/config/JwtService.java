00eae153198baac3cffadfd7956d2cc633bea7f0
devcom33 <devcom33@users.noreply.github.com>
2025-12-20 23:43:26 +0100

    feat: starting JwtService for jwt auth

---
+++ src/main/java/org/heymouad/bookingmanagementsystem/config/JwtService.java
@@ -0,0 +1,55 @@
+package org.heymouad.bookingmanagementsystem.config;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import io.jsonwebtoken.Claims;
+
+
+@Service
+public class JwtService {
+    private final static String SECRET_KEY = "0ece3e4916ba5bd2858a2fb0e4fe4586e1cb19ad5054a5be993fa8a7f0b8e499";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver)
+    {
+        final Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+    private Key getSigningKey() {
+        byte[] keybytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keybytes);
+    }
+}
