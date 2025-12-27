e778f4fe7047fccd7990890dfcbd90740413880f
devcom33 <devcom33@users.noreply.github.com>
2025-12-27 14:15:02 +0100

    Feat: getUser By Email

--- src/main/java/org/heymouad/bookingmanagementsystem/services/UserService.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/services/UserService.java
@@ -3,10 +3,12 @@ package org.heymouad.bookingmanagementsystem.services;
 import org.heymouad.bookingmanagementsystem.entities.User;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public interface UserService {
     User createUser(User user);
     User getUserById(UUID id);
+    User getUserByEmail(String email);
 }
