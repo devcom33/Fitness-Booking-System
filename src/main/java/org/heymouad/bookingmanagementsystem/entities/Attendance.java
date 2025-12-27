9704a4d06885554bebdc368cbf611f1078e439c8
devcom33 <devcom33@users.noreply.github.com>
2025-12-27 14:16:09 +0100

    Feat: add Attendance Entity

--- src/main/java/org/heymouad/bookingmanagementsystem/entities/Attendance.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/entities/Attendance.java
@@ -4,6 +4,7 @@ package org.heymouad.bookingmanagementsystem.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Attendance {
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -21,7 +23,7 @@ public class Attendance {
 
     @OneToOne
     @JoinColumn(name = "booking_id")
-    private Bookings booking;
+    private Booking booking;
 
     @CreatedDate
     private Instant attendanceAt;
