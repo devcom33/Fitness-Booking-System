cd4ecc7fd12ffe8098c7e58279ff445645c5d0b4
devcom33 <devcom33@users.noreply.github.com>
2025-12-27 14:12:59 +0100

    feat: add Authentification Controller

---
+++ src/main/java/org/heymouad/bookingmanagementsystem/controllers/Auth/AuthenticationController.java
@@ -0,0 +1,32 @@
+package org.heymouad.bookingmanagementsystem.controllers.Auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.heymouad.bookingmanagementsystem.dtos.UserRegistrationRequestDto;
+import org.heymouad.bookingmanagementsystem.dtos.auth.AuthRequestDto;
+import org.heymouad.bookingmanagementsystem.dtos.auth.AuthResponseDto;
+import org.heymouad.bookingmanagementsystem.services.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/auth")
+@RestController
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDto> register(@RequestBody UserRegistrationRequestDto userRegistrationRequestDto)
+    {
+        return ResponseEntity.ok(authenticationService.register(userRegistrationRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody AuthRequestDto authRequestDto)
+    {
+        return ResponseEntity.ok(authenticationService.login(authRequestDto));
+    }
+}
