3ca21762a774a127f7ae9976c74ac7b2ae1a136e
devcom33 <devcom33@users.noreply.github.com>
2025-12-29 00:55:45 +0100

    chore: add entity listner for auditing

--- src/main/java/org/heymouad/bookingmanagementsystem/entities/Instructor.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/entities/Instructor.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -16,6 +17,7 @@ import java.util.UUID;
 @Builder
 @Entity
 @Table(name = "instructors")
+@EntityListeners(AuditingEntityListener.class)
 public class Instructor {
 
     @Id
