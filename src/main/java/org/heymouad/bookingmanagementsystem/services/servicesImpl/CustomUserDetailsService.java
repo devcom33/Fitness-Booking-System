fefe1a4b4a3c7aa2a9525e8f5962d2cc3e98d753
devcom33 <devcom33@users.noreply.github.com>
2025-12-29 00:54:10 +0100

    chore: add Custom user details

---
+++ src/main/java/org/heymouad/bookingmanagementsystem/services/servicesImpl/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package org.heymouad.bookingmanagementsystem.services.servicesImpl;
+
+import lombok.RequiredArgsConstructor;
+import org.heymouad.bookingmanagementsystem.entities.User;
+import org.heymouad.bookingmanagementsystem.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found: "+ username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("ROLE_" + user.getRole().getName())
+                .build();
+    }
+}
