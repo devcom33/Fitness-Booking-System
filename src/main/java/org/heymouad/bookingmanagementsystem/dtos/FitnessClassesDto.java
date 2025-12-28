e3279f4aeee42667717660ad42789cd49f6ff546
devcom33 <devcom33@users.noreply.github.com>
2025-12-29 00:58:26 +0100

    chore: remove id from FitnessClassesDto

--- src/main/java/org/heymouad/bookingmanagementsystem/dtos/FitnessClassesDto.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/dtos/FitnessClassesDto.java
@@ -1,9 +1,6 @@
 package org.heymouad.bookingmanagementsystem.dtos;
 
-import java.util.UUID;
-
 public record FitnessClassesDto(
-        UUID id,
         String name,
         String description,
         int durationMinutes,
