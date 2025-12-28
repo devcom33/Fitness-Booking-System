54056ae21bfddb88b39e72f04db8a3aa546193c4
devcom33 <devcom33@users.noreply.github.com>
2025-12-29 00:53:12 +0100

    chore: add findByName in role repo

--- src/main/java/org/heymouad/bookingmanagementsystem/repositories/RoleRepository.java
+++ src/main/java/org/heymouad/bookingmanagementsystem/repositories/RoleRepository.java
@@ -1,11 +1,14 @@
 package org.heymouad.bookingmanagementsystem.repositories;
 
 import org.heymouad.bookingmanagementsystem.entities.Role;
+import org.heymouad.bookingmanagementsystem.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findByName(UserRole name);
 }
