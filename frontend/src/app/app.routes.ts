79b36f8b2a6eb73987388d073ed6ddad0843f591
devcom33 <devcom33@users.noreply.github.com>
2026-01-20 16:53:45 +0100

    fix: add paths routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { Dashboard } from './dashboard/dashboard';
+import { Classes } from './classes/classes';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {
@@ -10,5 +13,14 @@ export const routes: Routes = [
   {
     path: 'register',
     component: Register,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'browse-classes',
+    component: Classes,
+    canActivate: [authGuard],
   },
 ];
