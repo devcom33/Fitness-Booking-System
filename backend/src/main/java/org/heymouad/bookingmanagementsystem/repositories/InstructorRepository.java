0a30fb5235ecad5073d1a0837d73e9a58722cc22
devcom33 <devcom33@users.noreply.github.com>
2026-02-21 23:30:35 +0000

    feat: add method to find instructors by user status

--- backend/src/main/java/org/heymouad/bookingmanagementsystem/repositories/InstructorRepository.java
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/repositories/InstructorRepository.java
@@ -1,13 +1,23 @@
 package org.heymouad.bookingmanagementsystem.repositories;
 
 import org.heymouad.bookingmanagementsystem.entities.Instructor;
+import org.heymouad.bookingmanagementsystem.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface InstructorRepository extends JpaRepository<Instructor, UUID> {
     Optional<Instructor> findByUserEmail(String email);
+    @Query("""
+        SELECT i
+            FROM Instructor i
+                WHERE i.user.userStatus = :status
+    """)
+    List<Instructor> findAllByUserStatus(@Param("status")UserStatus status);
 }
