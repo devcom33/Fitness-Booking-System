5ece433bad3a762a130af41837f0431cad1c9dca
devcom33 <devcom33@users.noreply.github.com>
2026-02-21 23:30:54 +0000

    feat: add AdminService interface for managing instructor statuses

---
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/services/AdminService.java
@@ -0,0 +1,12 @@
+package org.heymouad.bookingmanagementsystem.services;
+
+import org.heymouad.bookingmanagementsystem.dtos.instructor.InstructorResponseDto;
+import org.heymouad.bookingmanagementsystem.enums.UserStatus;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AdminService {
+    void updateInstructorStatus(UUID instructorId, UserStatus newStatus);
+    List<InstructorResponseDto> getPendingInstructors();
+}
