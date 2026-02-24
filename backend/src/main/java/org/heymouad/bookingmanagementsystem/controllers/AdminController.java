1db074c4ef245569d3a3804e1aae00a648043a5f
devcom33 <devcom33@users.noreply.github.com>
2026-02-24 23:49:09 +0000

    feat: add AdminController for managing instructor status updates and retrieving pending instructors

---
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/controllers/AdminController.java
@@ -0,0 +1,42 @@
+package org.heymouad.bookingmanagementsystem.controllers;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.heymouad.bookingmanagementsystem.dtos.UserStatusUpdateRequest;
+import org.heymouad.bookingmanagementsystem.dtos.instructor.InstructorResponseDto;
+import org.heymouad.bookingmanagementsystem.enums.UserStatus;
+import org.heymouad.bookingmanagementsystem.mappers.InstructorMapper;
+import org.heymouad.bookingmanagementsystem.services.AdminService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@RequestMapping("/api/v1/admin")
+@RestController
+@RequiredArgsConstructor
+public class AdminController {
+    private final AdminService adminService;
+    private final InstructorMapper instructorMapper;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PatchMapping("/instructors/{instructorId}/status")
+    public ResponseEntity<Void> updateInstructorStatus(@PathVariable UUID instructorId, @RequestBody @Valid UserStatusUpdateRequest userStatusUpdateRequest)
+    {
+        UserStatus userStatus = UserStatus.valueOf(userStatusUpdateRequest.userStatus());
+        adminService.updateInstructorStatus(instructorId, userStatus);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/instructors/pending")
+    public ResponseEntity<List<InstructorResponseDto>> getPendingInstructor()
+    {
+        return ResponseEntity.ok(adminService.getPendingInstructors());
+    }
+
+}
