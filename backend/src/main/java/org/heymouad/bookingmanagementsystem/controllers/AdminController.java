f1a6929d432189bda21797f5d54f72178bf9035a
devcom33 <devcom33@users.noreply.github.com>
2026-02-26 22:54:55 +0000

    fix: specify response type for admin API endpoint

--- backend/src/main/java/org/heymouad/bookingmanagementsystem/controllers/AdminController.java
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/controllers/AdminController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 
-@RequestMapping("/api/v1/admin")
+@RequestMapping(path = "/api/v1/admin", produces = "application/json")
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
