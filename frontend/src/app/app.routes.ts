8d229eaacc60f719c414e9aafe1dd183e6a70c0a
devcom33 <devcom33@users.noreply.github.com>
2026-02-26 22:52:25 +0000

    feat: update routing to include admin trainers component with auth guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { Login } from './shared/login/login';
 import { Register } from './shared/register/register';
 import { Dashboard } from './shared/dashboard/dashboard';
-import { authGuard } from './guards/auth-guard';
+import { authGuard } from './core/guards/auth-guard';
 import { ClassList } from './client/class-list/class-list';
 import { MyBookings } from './client/my-bookings/my-bookings';
 import { Landing } from './landing/landing';
 import { Schedule } from './trainer/schedule/schedule';
 import { Unauthorized } from './shared/unauthorized/unauthorized';
 import { FitnessClassStepper } from './trainer/fitness-class-stepper/fitness-class-stepper';
+import { Trainers } from './admin/trainers/trainers';
 
 export const routes: Routes = [
   {
@@ -35,11 +36,13 @@ export const routes: Routes = [
     path: 'browse-classes',
     component: ClassList,
     canActivate: [authGuard],
+    data: { expectedRole: 'ROLE_CLIENT' }
   },
   {
     path: 'my-bookings',
     component: MyBookings,
     canActivate: [authGuard],
+    data: { expectedRole: 'ROLE_CLIENT' }
   },
   {
     path: 'trainer/schedule',
@@ -52,5 +55,11 @@ export const routes: Routes = [
     component: FitnessClassStepper,
     canActivate: [authGuard],
     data: { expectedRole: 'ROLE_INSTRUCTOR' },
+  },
+  {
+    path: 'admin/trainers',
+    component: Trainers,
+    canActivate: [authGuard],
+    data: { expectedRole: 'ROLE_ADMIN' },
   },
 ];
