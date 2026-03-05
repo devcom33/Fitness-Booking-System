75cc709ad27d6423517ab19041bd8bc092346e34
devcom33 <devcom33@users.noreply.github.com>
2026-03-05 16:34:11 +0000

    feat: update AdminController to manage instructor onboarding and account access also update endpoints names

--- backend/src/main/java/org/heymouad/bookingmanagementsystem/controllers/AdminController.java
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/controllers/AdminController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.heymouad.bookingmanagementsystem.dtos.UserStatusUpdateRequest;
 import org.heymouad.bookingmanagementsystem.dtos.instructor.InstructorResponseDto;
 import org.heymouad.bookingmanagementsystem.enums.UserStatus;
-import org.heymouad.bookingmanagementsystem.mappers.InstructorMapper;
 import org.heymouad.bookingmanagementsystem.services.AdminService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,34 +15,39 @@ import java.util.List;
 import java.util.UUID;
 
 
-@RequestMapping(path = "/api/v1/admin", produces = "application/json")
+@RequestMapping(path = "/api/v1/admin/instructors", produces = "application/json")
 @RestController
+@PreAuthorize("hasRole('ADMIN')")
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
-    private final InstructorMapper instructorMapper;
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @PatchMapping("/instructors/{instructorId}/status")
-    public ResponseEntity<Void> updateInstructorStatus(@PathVariable UUID instructorId, @RequestBody @Valid UserStatusUpdateRequest userStatusUpdateRequest)
+    @GetMapping
+    public ResponseEntity<List<InstructorResponseDto>> getInstructors(@RequestParam(required = false, defaultValue = "ACTIVE") UserStatus status)
     {
-        UserStatus userStatus = UserStatus.valueOf(userStatusUpdateRequest.userStatus());
+        List<InstructorResponseDto> instructors = switch (status) {
+            case PENDING -> adminService.getPendingInstructors();
+            case BLOCKED -> adminService.getDeactivatedInstructors();
+            default -> adminService.getActivatedInstructors();
+        };
+
+        return ResponseEntity.ok(instructors);
+    }
+
+    @PatchMapping("/{instructorId}/onboarding")
+    public ResponseEntity<Void> approveInstructor(@PathVariable UUID instructorId, @RequestBody @Valid UserStatusUpdateRequest userStatusUpdateRequest)
+    {
+        UserStatus userStatus = userStatusUpdateRequest.userStatus();
         adminService.updateInstructorStatus(instructorId, userStatus);
         return ResponseEntity.noContent().build();
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/instructors/pending")
-    public ResponseEntity<List<InstructorResponseDto>> getPendingInstructor()
+    @PatchMapping("/instructors/{instructorId}/access")
+    public ResponseEntity<Void> toggleAccountAccess(@PathVariable UUID instructorId, @RequestBody @Valid UserStatusUpdateRequest userStatusUpdateRequest)
     {
-        return ResponseEntity.ok(adminService.getPendingInstructors());
-    }
-
-    @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/instructors")
-    public ResponseEntity<List<InstructorResponseDto>> getInstructors()
-    {
-        return ResponseEntity.ok(adminService.getInstructors());
+        UserStatus userStatus = userStatusUpdateRequest.userStatus();
+        adminService.updateAccountState(instructorId, userStatus);
+        return ResponseEntity.noContent().build();
     }
 
 }
