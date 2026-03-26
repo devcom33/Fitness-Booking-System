bd0aea39be9a416fe35c067c29d0c9a752fa6181
devcom33 <devcom33@users.noreply.github.com>
2026-03-26 23:35:35 +0100

    feat: add ScheduleStatus field to ClassScheduleResponseDto for schedule state management

--- backend/src/main/java/org/heymouad/bookingmanagementsystem/dtos/ClassScheduleResponseDto.java
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/dtos/ClassScheduleResponseDto.java
@@ -3,6 +3,7 @@ package org.heymouad.bookingmanagementsystem.dtos;
 
 import lombok.Builder;
 import org.heymouad.bookingmanagementsystem.dtos.instructor.InstructorDto;
+import org.heymouad.bookingmanagementsystem.enums.ScheduleStatus;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
@@ -11,6 +12,7 @@ import java.util.UUID;
 public record ClassScheduleResponseDto(UUID id,
                                        FitnessClassesDto fitnessClassDto,
                                        InstructorDto instructorDto,
+                                       ScheduleStatus status,
                                        ZonedDateTime startTime,
                                        ZonedDateTime endTime,
                                        RecurringScheduleTemplateDto templateDto) {
