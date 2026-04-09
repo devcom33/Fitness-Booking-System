a14ef211f357801e32996cf78788cf0b5aa548f9
devcom33 <devcom33@users.noreply.github.com>
2026-04-09 22:19:15 +0100

    feat: add AdminDashboardController for fetching dashboard statistics

---
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/controllers/AdminDashboardController.java
@@ -0,0 +1,24 @@
+package org.heymouad.bookingmanagementsystem.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.heymouad.bookingmanagementsystem.dtos.admin.DashboardStatsDTO;
+import org.heymouad.bookingmanagementsystem.services.AdminDashboardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping(path = "/api/v1/dashboard", produces = "application/json")
+@RestController
+@RequiredArgsConstructor
+public class AdminDashboardController {
+    private final AdminDashboardService adminDashboardService;
+
+    @GetMapping("/stats")
+    public ResponseEntity<DashboardStatsDTO> getDashboardStats() {
+        return ResponseEntity.ok(adminDashboardService.getDashboardStats());
+    }
+
+}
