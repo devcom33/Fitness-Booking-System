fb06284991083673d70ea7a27ba3fc2efa04e5fa
devcom33 <devcom33@users.noreply.github.com>
2026-04-11 22:13:08 +0100

    feat: add endpoint to fetch last month's booking statistics in AdminDashboardController and update AdminDashboardService interface

--- backend/src/main/java/org/heymouad/bookingmanagementsystem/controllers/AdminDashboardController.java
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/controllers/AdminDashboardController.java
@@ -2,12 +2,15 @@ package org.heymouad.bookingmanagementsystem.controllers;
 
 
 import lombok.RequiredArgsConstructor;
+import org.heymouad.bookingmanagementsystem.dtos.admin.BookingMonthlyCountDTO;
 import org.heymouad.bookingmanagementsystem.dtos.admin.DashboardStatsDTO;
 import org.heymouad.bookingmanagementsystem.services.AdminDashboardService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RequestMapping(path = "/api/v1/dashboard", produces = "application/json")
@@ -19,6 +22,11 @@ public class AdminDashboardController {
     @GetMapping("/stats")
     public ResponseEntity<DashboardStatsDTO> getDashboardStats() {
         return ResponseEntity.ok(adminDashboardService.getDashboardStats());
+    }
+
+    @GetMapping("/last-months-stats")
+    public ResponseEntity<List<BookingMonthlyCountDTO>> getLastMonthsStats() {
+        return ResponseEntity.ok(adminDashboardService.getLastMonthsStats(7));
     }
 
 }
--- backend/src/main/java/org/heymouad/bookingmanagementsystem/services/AdminDashboardService.java
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/services/AdminDashboardService.java
@@ -1,8 +1,12 @@
 package org.heymouad.bookingmanagementsystem.services;
 
+import org.heymouad.bookingmanagementsystem.dtos.admin.BookingMonthlyCountDTO;
 import org.heymouad.bookingmanagementsystem.dtos.admin.DashboardStatsDTO;
+
+import java.util.List;
 
 
 public interface AdminDashboardService {
     DashboardStatsDTO getDashboardStats();
+    List<BookingMonthlyCountDTO> getLastMonthsStats(int months);
 }
