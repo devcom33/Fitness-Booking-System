3a12ebaf4ef3eae4d7b015e5a0173986b8567a2d
devcom33 <devcom33@users.noreply.github.com>
2026-04-16 23:50:34 +0100

    chore: remove unused imports in BookingRepository

--- backend/src/main/java/org/heymouad/bookingmanagementsystem/repositories/BookingRepository.java
+++ backend/src/main/java/org/heymouad/bookingmanagementsystem/repositories/BookingRepository.java
@@ -12,10 +12,8 @@ import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Pageable;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @Repository
