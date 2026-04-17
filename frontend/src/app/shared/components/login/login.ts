f069d0ef64d57a9b51ef0b3f7d74f1435cfc710b
devcom33 <devcom33@users.noreply.github.com>
2026-04-17 21:53:22 +0100

    chore: enhance login process with toast notifications for user feedback

--- frontend/src/app/shared/components/login/login.ts
+++ frontend/src/app/shared/components/login/login.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { AuthenticationControllerService, AuthRequestDto } from '../../../api';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ToastService } from '../../services/toast-service';
 
 @Component({
   selector: 'app-login',
@@ -14,14 +15,31 @@ export class Login {
   private router = inject(Router);
   authRequest: AuthRequestDto = { email: '', password: '' };
   errorMsg: Array<String> = [];
+  private readonly toast = inject(ToastService);
 
   login() {
     localStorage.removeItem('token');
     this.authService.login(this.authRequest).subscribe({
       next: (res: any) => {
         localStorage.setItem('token', res.accessToken);
-        console.log('about to navigate ...');
+        this.toast.show('Welcome back!', 'success');
         this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        const backendCode = err?.error?.code;
+
+        if (backendCode === 'ACCOUNT_PENDING_APPROVAL') {
+          this.toast.show('Your instructor application is under review.', 'warning');
+          this.router.navigate(['/application-under-review']);
+          return;
+        }
+
+        if (backendCode === 'INVALID_CREDENTIALS') {
+          this.toast.show('Invalid email or password.', 'error');
+          return;
+        }
+
+        this.toast.show('Login failed. Please try again.', 'error');
       },
     });
   }
