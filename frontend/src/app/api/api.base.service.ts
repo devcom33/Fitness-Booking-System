ba9906cd7d834a15a2e56952c255b7a47b845abf
devcom33 <devcom33@users.noreply.github.com>
2026-04-19 16:45:23 +0100

    fix: remove default API base path

--- frontend/src/app/api/api.base.service.ts
+++ frontend/src/app/api/api.base.service.ts
@@ -13,7 +13,7 @@ import { Configuration } from './configuration';
 import { OpenApiHttpParams, QueryParamStyle, concatHttpParamsObject} from './query.params';
 
 export class BaseService {
-    protected basePath = '/api';
+    protected basePath = '';
     public defaultHeaders = new HttpHeaders();
     public configuration: Configuration;
     public encoder: HttpParameterCodec;
