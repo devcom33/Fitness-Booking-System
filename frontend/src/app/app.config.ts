8abbbe1bd019fd8535c04205535aa137a833d60f
devcom33 <devcom33@users.noreply.github.com>
2026-04-19 16:04:08 +0100

    feat: update API configuration to use relative path for root URL

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from '../app/core/interceptors/auth.interceptor';
+import { provideApiConfiguration } from './api/api-configuration';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes),
+    provideApiConfiguration('/api')
   ],
 };
