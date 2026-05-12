f2da20b2802c5b1d24af30823379b17627aa3531
devcom33 <devcom33@users.noreply.github.com>
2026-05-12 17:14:26 +0100

    tests: enhance and fix FitnessClassServiceImplTest with category handling for create and update methods

--- backend/src/test/java/org/heymouad/bookingmanagementsystem/services/servicesImpl/FitnessClassServiceImplTest.java
+++ backend/src/test/java/org/heymouad/bookingmanagementsystem/services/servicesImpl/FitnessClassServiceImplTest.java
@@ -1,8 +1,10 @@
 package org.heymouad.bookingmanagementsystem.services.servicesImpl;
 
 
+import org.heymouad.bookingmanagementsystem.entities.Category;
 import org.heymouad.bookingmanagementsystem.entities.FitnessClass;
 import org.heymouad.bookingmanagementsystem.exceptions.ResourceNotFoundException;
+import org.heymouad.bookingmanagementsystem.repositories.CategoryRepository;
 import org.heymouad.bookingmanagementsystem.repositories.FitnessClassRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,9 +16,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class FitnessClassServiceImplTest {
@@ -26,50 +30,73 @@ public class FitnessClassServiceImplTest {
     @InjectMocks
     private FitnessClassServiceImpl fitnessClassService;
 
+    @Mock
+    private CategoryRepository categoryRepository;
+
 
     @Test
     void createFitnessClasses_shouldSaveAndReturnEntity() {
+        //Given
+        String categoryName = "HIIT";
+        Category mockCategory = Category.builder().name(categoryName).build();
+
         FitnessClass fitnessClass = FitnessClass.builder()
-                .name("Yoga")
+                .name("Gym")
                 .capacity(20)
                 .durationMinutes(60)
-                .category("Wellness")
                 .build();
 
-        when(fitnessClassRepository.save(fitnessClass))
+        when(categoryRepository.findCategoryByName(categoryName))
+                .thenReturn(Optional.of(mockCategory));
+
+        when(fitnessClassRepository.save(any(FitnessClass.class)))
                 .thenReturn(fitnessClass);
 
-        FitnessClass result = fitnessClassService.createFitnessClasses(fitnessClass);
+        FitnessClass result = fitnessClassService.createFitnessClasses(fitnessClass, categoryName);
 
         assertNotNull(result);
-        assertEquals("Yoga", result.getName());
-        verify(fitnessClassRepository).save(fitnessClass);
+        assertEquals("Gym", result.getName());
+
+        verify(categoryRepository).findCategoryByName(categoryName);
+        verify(fitnessClassRepository).save(any(FitnessClass.class));
     }
+
     @Test
     void updateFitnessClasses_shouldUpdateAndReturnEntity() {
+        //Given
+        String categoryName = "HIIT";
+        String updatedCategoryName = "Cardio";
+
+        Category mockUpdatedCategoryName = Category.builder().name(updatedCategoryName).build();
+
         UUID id = UUID.randomUUID();
+
         FitnessClass existingClass = FitnessClass.builder()
                 .id(id)
-                .name("Pilates")
+                .name("GYM")
                 .capacity(15)
                 .durationMinutes(45)
-                .category("Fitness")
                 .build();
 
         FitnessClass updatedInfo = FitnessClass.builder()
-                .name("Advanced Pilates")
+                .name("Advanced Gym")
                 .capacity(20)
                 .durationMinutes(50)
-                .category("Advanced Fitness")
                 .build();
 
         when(fitnessClassRepository.findById(id)).thenReturn(Optional.of(existingClass));
+        when(categoryRepository.findCategoryByName(updatedCategoryName))
+                .thenReturn(Optional.of(mockUpdatedCategoryName));
         when(fitnessClassRepository.save(existingClass)).thenReturn(existingClass);
 
-        FitnessClass updatedClass = fitnessClassService.updateFitnessClasses(id, updatedInfo);
+        FitnessClass updatedClass = fitnessClassService.updateFitnessClasses(id, updatedInfo, updatedCategoryName);
+
         assertNotNull(updatedClass);
-        assertEquals("Advanced Pilates", updatedClass.getName());
+        assertEquals("Advanced Gym", updatedClass.getName());
         assertEquals(20, updatedClass.getCapacity());
+        assertEquals(updatedCategoryName, updatedClass.getCategory().getName());
+
+        verify(categoryRepository).findCategoryByName(updatedCategoryName);
         verify(fitnessClassRepository).findById(id);
         verify(fitnessClassRepository).save(existingClass);
     }
@@ -77,45 +104,53 @@ public class FitnessClassServiceImplTest {
     @Test
     void updateFitnessClasses_shouldThrowExceptionWhenNotFound(){
         UUID id = UUID.randomUUID();
+        String updatedCategoryName = "Cardio";
 
         FitnessClass updatedInfo = FitnessClass.builder()
-                .name("Advanced Pilates")
+                .name("Advanced Gym")
                 .capacity(20)
                 .durationMinutes(50)
-                .category("Advanced Fitness")
                 .build();
 
         when(fitnessClassRepository.findById(id)).thenReturn(Optional.empty());
-        assertThrows(ResourceNotFoundException.class,
-                () -> fitnessClassService.updateFitnessClasses(id, updatedInfo));
 
+        assertThatThrownBy(() -> fitnessClassService.updateFitnessClasses(id, updatedInfo, updatedCategoryName))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessageContaining("Fitness Class Not Found with id");
+
+        verify(fitnessClassRepository, never()).save(any());
         verify(fitnessClassRepository).findById(id);
-
     }
 
 
     @Test
     public void getFitnessClassesById_shouldReturnEntity() {
+        String categoryName = "HIIT";
+        Category mockCategory = Category.builder().name(categoryName).build();
+
         UUID id = UUID.randomUUID();
 
         FitnessClass existing = FitnessClass.builder()
                 .id(id)
-                .name("Pilates")
+                .name("Gym")
                 .capacity(15)
                 .durationMinutes(45)
-                .category("Fitness")
+                .category(mockCategory)
                 .build();
 
         when(fitnessClassRepository.findById(id)).thenReturn(Optional.of(existing));
 
         FitnessClass fitnessClass = fitnessClassService.getFitnessClassesById(id);
-        assertNotNull(fitnessClass);
-        assertEquals("Pilates", fitnessClass.getName());
-        assertEquals(15, fitnessClass.getCapacity());
-        assertEquals(45, fitnessClass.getDurationMinutes());
-        assertEquals("Fitness", fitnessClass.getCategory());
 
-        // verify - It ensures your service does its job properly by using the repository, not by cheating with hardcoded values
+        assertThat(fitnessClass)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("name", "Gym")
+                .hasFieldOrPropertyWithValue("capacity", 15)
+                .hasFieldOrPropertyWithValue("durationMinutes", 45)
+                .extracting(FitnessClass::getCategory)
+                .hasFieldOrPropertyWithValue("name", "HIIT");
+
+        // verify
         verify(fitnessClassRepository).findById(id);
     }
 
@@ -125,8 +160,9 @@ public class FitnessClassServiceImplTest {
 
         when(fitnessClassRepository.findById(id)).thenReturn(Optional.empty());
 
-        assertThrows(ResourceNotFoundException.class,
-                () -> fitnessClassService.getFitnessClassesById(id));
+        assertThatThrownBy(() -> fitnessClassService.getFitnessClassesById(id))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessageContaining(id.toString());
 
         verify(fitnessClassRepository).findById(id);
     }
@@ -135,34 +171,40 @@ public class FitnessClassServiceImplTest {
     @Test
     public void getAllFitnessClasses_shouldReturnAllEntities()
     {
+        String categoryName = "HIIT";
+        String categoryName_2 = "Cardio";
+
+        Category mockCategoryName = Category.builder().name(categoryName).build();
+        Category mockCategoryName_2 = Category.builder().name(categoryName_2).build();
+
         UUID id = UUID.randomUUID();
 
         FitnessClass class1 = FitnessClass.builder()
                 .id(id)
-                .name("Pilates")
+                .name("Gym")
                 .capacity(15)
                 .durationMinutes(45)
-                .category("Fitness")
+                .category(mockCategoryName)
                 .build();
 
         UUID id2 = UUID.randomUUID();
 
         FitnessClass class2 = FitnessClass.builder()
                 .id(id2)
-                .name("Advanced Pilates")
+                .name("Advanced Gym")
                 .capacity(20)
                 .durationMinutes(50)
-                .category("Advanced Fitness")
+                .category(mockCategoryName_2)
                 .build();
 
         when(fitnessClassRepository.findAll()).thenReturn(List.of(class1, class2));
 
         List<FitnessClass> fitnessClassList = fitnessClassService.getAllFitnessClasses();
 
-        assertNotNull(fitnessClassList);
-        assertEquals(2, fitnessClassList.size());
-        assertEquals(class1, fitnessClassList.get(0));
-        assertEquals(class2, fitnessClassList.get(1));
+        assertThat(fitnessClassList)
+                .hasSize(2)
+                .containsExactly(class1, class2)
+                .doesNotContainNull();
 
         verify(fitnessClassRepository).findAll();
     }
